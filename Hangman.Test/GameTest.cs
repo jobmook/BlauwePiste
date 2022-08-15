8800c99aa7c1eff529c1ea653f8156e820d8871f
Job Mook <jobmook@live.nl>
2022-08-15 10:11:28 +0200

    Finished RazorApp with ASP.NET Core frontend

---
+++ Hangman.Test/GameTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman.Test
+{
+    [TestClass]
+    public class GameTest
+    {
+
+
+    }
+}
